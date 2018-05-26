e5958e1c080f7cd9be543d86122dba2edecf43a0
Rodrigo de Oliveira Fernandes <rodrigodeoliveira.ti@gmail.com>
2018-05-26 17:02:17 -0300

    Add the total time calculation for a given piloto

--- racestats.py
+++ racestats.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime as dt
 
 
 def ParseLog(log):
@@ -47,3 +48,24 @@ def GetLastLapData(piloto_race_data):
     last_lap_results.sort()
 
     return last_lap_results
+
+
+def CalculatePilotoTotalTime(race_data, cod_piloto):
+
+    time_list = [dt.datetime.strptime(x[-2], '%M:%S.%f')
+                 for x in race_data if x[1] == cod_piloto]
+    minutes_sum, seconds_sum, microseconds_sum = (0, 0, 0)
+
+    for time in time_list:
+        minutes_sum += time.minute
+        seconds_sum += time.second
+        microseconds_sum += time.microsecond
+
+    total_time = dt.timedelta(
+        minutes=minutes_sum,
+        seconds=seconds_sum,
+        microseconds=microseconds_sum
+    )
+
+    return str(total_time)[:-3]
+
