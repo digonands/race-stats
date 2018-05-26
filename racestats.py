eec903a2fbcb2dc0048c5b49655603195debb723
Rodrigo de Oliveira Fernandes <rodrigodeoliveira.ti@gmail.com>
2018-05-26 18:01:17 -0300

    Add the main function and some comments to the source code

--- racestats.py
+++ racestats.py
@@ -3,34 +3,47 @@ import datetime as dt
 
 
 def ParseLog(log):
-
+    """
+    Process a log file with a race data
+    """
     race_data = []
 
+    # Read the lines from log except the heading line
+    # Splits each line to get a list of values
+    # Cleanup the line with unwanted stuff
     for line in log.splitlines()[1:]:
         line = line.split()
         line.remove(line[2])
+
         race_data.append(line)
 
     return race_data
 
 
 def ParseCodPilotos(log):
-
+    """
+    Process a log file and get a list of the drivers from the race
+    """
     cod_pilotos = []
 
+    # Get all cod_pilotos (line[1]) and append to a list
     for line in log.splitlines()[1:]:
         line = line.split()
         cod_pilotos.append(line[1])
 
+    # Use set to get unique values then convert to a list again
     cod_pilotos = list(set(cod_pilotos))
 
     return cod_pilotos
 
 
 def RaceDataByPiloto(race_data, cod_pilotos):
-
+    """
+    Group the data from race by driver
+    """
     piloto_race_data = []
 
+    # Use cod_pilotos (x[1]) to group the race data
     for cod_piloto in cod_pilotos:
         piloto_race_data.append([x for x in race_data if x[1] == cod_piloto])
 
@@ -38,22 +51,33 @@ def RaceDataByPiloto(race_data, cod_pilotos):
 
 
 def GetLastLapData(piloto_race_data):
-
+    """
+    Get only the last lap data from each driver
+    """
     last_lap_results = []
 
     for p in piloto_race_data:
         last_lap = p[-1]
         last_lap_results.append(last_lap)
 
+    # Make the results appear in correct order
     last_lap_results.sort()
 
     return last_lap_results
 
 
 def CalculatePilotoTotalTime(race_data, cod_piloto):
+    """
+    Calculate the total time for a driver
+    """
 
+    # The time comes from race_data and it is converted to datetime format
+    # The time list is selected for the given driver
     time_list = [dt.datetime.strptime(x[-2], '%M:%S.%f')
                  for x in race_data if x[1] == cod_piloto]
+
+    # The minutes, seconds and microseconds should be sum separately
+    # Then the total time is calculated by the timedelta function
     minutes_sum, seconds_sum, microseconds_sum = (0, 0, 0)
 
     for time in time_list:
@@ -67,11 +91,14 @@ def CalculatePilotoTotalTime(race_data, cod_piloto):
         microseconds=microseconds_sum
     )
 
+    # Convert to string and remove the unwanted '000'
     return str(total_time)[:-3]
 
 
 def ShowRaceResults(race_data, last_lap_data):
-
+    """
+    Format and print the race results
+    """
     header = "Posição\t\tCódigo Piloto\tNome Piloto\tVoltas Completadas\tTempo Total"
     print(header)
     posicao = 1
@@ -83,3 +110,23 @@ def ShowRaceResults(race_data, last_lap_data):
             posicao, r[1], r[2], r[3], total_time))
         posicao += 1
 
+
+def main():
+    """
+    The main program.
+    Reads the log file then show the race results
+    """
+    with open('race.log') as file:
+        read_data = file.read()
+
+    parsed_log = ParseLog(read_data)
+    cod_pilotos = ParseCodPilotos(read_data)
+    pilotos_data = RaceDataByPiloto(parsed_log, cod_pilotos)
+    last_lap_data = GetLastLapData(pilotos_data)
+
+    print("{} is the winner!\n".format(last_lap_data[0][2]))
+    ShowRaceResults(parsed_log, last_lap_data)
+
+
+if __name__ == "__main__":
+    main()
