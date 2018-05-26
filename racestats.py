b2f4275a2d4f499f10f35745fcfbc2f1d9df2ef0
Rodrigo de Oliveira Fernandes <rodrigodeoliveira.ti@gmail.com>
2018-05-26 12:14:27 -0300

    Create functions to load the race results

--- racestats.py
+++ racestats.py
@@ -24,3 +24,26 @@ def ParseCodPilotos(log):
     cod_pilotos = list(set(cod_pilotos))
 
     return cod_pilotos
+
+
+def RaceDataByPiloto(race_data, cod_pilotos):
+
+    piloto_race_data = []
+
+    for cod_piloto in cod_pilotos:
+        piloto_race_data.append([x for x in race_data if x[1] == cod_piloto])
+
+    return piloto_race_data
+
+
+def GetLastLapData(piloto_race_data):
+
+    last_lap_results = []
+
+    for p in piloto_race_data:
+        last_lap = p[-1]
+        last_lap_results.append(last_lap)
+
+    last_lap_results.sort()
+
+    return last_lap_results
